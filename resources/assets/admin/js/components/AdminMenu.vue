26772740360eac2d4a523948ec9b77d8eb42862f
Ivan Santos <ivannildosanto@hotmail.com>
2016-11-25 16:43:25 -0300

    Configurando menus administrativos

--- resources/assets/admin/js/components/AdminMenu.vue
+++ resources/assets/admin/js/components/AdminMenu.vue
@@ -1,34 +1,32 @@
 <template>
-    <ul :id="o.id" class="dropdown-content" v-for="o in menusDropdown">
+    <ul :id="o.id" class="dropdown-content" v-for="o in config.menusDropdown">
         <li v-for="item in o.items">
-            <a v-link="{name: item.routeName}">{{ item.name }}</a>
+            <a :href="item.url">{{ item.name }}</a>
         </li>
     </ul>
     <div class="navbar-fixed">
-        <!--<nav class="teal">-->
-        <nav>
+        <nav class="teal">
             <div class="nav-wrapper container ">
                 <a href="#" class="right brand-logo">PR Contas</a>
                 <a href="#" data-activates="nav-mobile" class="button-collapse">
                     <i class="material-icons">menu</i>
                 </a>
                 <ul class="left hide-on-med-and-down">
-                    <li v-for="o in menus">
+                    <li v-for="o in config.menus">
                         <a v-if="o.bropdownId" class="dropdown-button" href="!#" :data-activates="o.bropdownId" >
                             {{ o.name }} <i class="material-icons right">arrow_drop_down</i>
                         </a>
-                        <a v-else v-link="{name: o.routeName}">{{ o.name }}</a>
+                        <a v-else :href="o.url">{{ o.name }}</a>
                     </li>
                 </ul>
                 <ul id="nav-mobile" class="side-nav">
-                    <li v-for="o in menus">
-                        <a v-link="{ name: o.routeName}">{{ o.name }}</a>
+                    <li v-for="o in config.menus">
+                        <a :href="o.url">{{ o.name }}</a>
                     </li>
                 </ul>
             </div>
         </nav>
     </div>
-    <router-view></router-view>
 </template>
 
 <script type='text/javascript' >
@@ -49,29 +47,6 @@
         ready() {
             $('.button-collapse').sideNav();
             $('.dropdown-button').dropdown();
-        },
-        data() {
-            return {
-                menus: [
-                    {name: 'Dashboard', routeName: '/dashboard'},
-                    {name: 'Contas a pagar', routeName: 'bill-pay.list', bropdownId: 'bill-pay'},
-                    {name: 'Contas a receber', routeName: 'bill-receive.list', bropdownId: 'bill-receive'}
-                ],
-                menusDropdown: [
-                    {
-                        id: 'bill-pay', items: [
-                        {id: 0, name: 'Listar contas', routeName: 'bill-pay.list'},
-                        {id: 1, name: 'Criar contas', routeName: 'bill-pay.create'}
-                    ]
-                    },
-                    {
-                        id: 'bill-receive', items: [
-                        {id: 0, name: 'Listar contas', routeName: 'bill-receive.list'},
-                        {id: 1, name: 'Criar contas', routeName: 'bill-receive.create'}
-                    ]
-                    }
-                ]
-            };
         }
     };
 </script>
