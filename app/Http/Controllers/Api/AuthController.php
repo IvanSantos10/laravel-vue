4a4944bc4abfd90e91ec4502c0b31df51537bb4f
Ivan Santos <ivannildosanto@hotmail.com>
2017-05-28 17:19:13 -0300

    Tratamento de erros de login

--- app/Http/Controllers/Api/AuthController.php
+++ app/Http/Controllers/Api/AuthController.php
@@ -6,6 +6,7 @@ use financeiro\Http\Controllers\Controller;
 use financeiro\User;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 use Illuminate\Http\Request;
+use Lang;
 
 class AuthController extends Controller
 {
@@ -16,18 +17,53 @@ class AuthController extends Controller
     {
         $this->validateLogin($request);
 
+        // If the class is using the ThrottlesLogins trait, we can automatically throttle
+        // the login attempts for this application. We'll key this by the username and
+        // the IP address of the client making these requests into this application.
+        if ($this->hasTooManyLoginAttempts($request)) {
+            $this->fireLockoutEvent($request);
+
+            return $this->sendLockoutResponse($request);
+        }
+
         $credentials = $this->credentials($request);
 
         if ($token = \Auth::guard('api')->attempt($credentials)) {
             return $this->sendLoginResponse($request, $token);
         }
+
+        $this->incrementLoginAttempts($request);
+
+        return $this->sendFailedLoginResponse($request);
     }
 
     protected function sendLoginResponse(Request $request, $token)
     {
+        $this->clearLoginAttempts($request);
+
         return response()->json([
             'token' => $token
         ]);
+    }
+
+    protected function sendLockoutResponse(Request $request)
+    {
+        $seconds = $this->limiter()->availableIn(
+            $this->throttleKey($request)
+        );
+
+        $message = Lang::get('auth.throttle', ['seconds' => $seconds]);
+
+        return response()->json([
+            'message' => $message
+        ],403);
+    }
+
+    protected function sendFailedLoginResponse(Request $request)
+    {
+        return response()->json([
+            'message' => Lang::get('auth.failed')
+        ],401);
     }
 
 
