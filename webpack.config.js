1a81b255b5809d74a71f20b4d018782d52e4d26b
Ivan Santos <ivannildosanto@hotmail.com>
2017-05-31 23:30:33 -0300

    Criando layout do SPA

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ var webpack = require('webpack');
 
 module.exports = {
     entry: {
-         admin: './resources/assets/admin/js/admin.js'
+         admin: './resources/assets/admin/js/admin.js',
          spa: './resources/assets/spa/js/spa.js'
     },
     output: {
