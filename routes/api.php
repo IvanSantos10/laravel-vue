92e4bcd8ba780e0867167eb46d751c431da06e82
Ivan Santos <ivannildosanto@hotmail.com>
2017-06-03 18:47:06 -0300

    Organizando rotas da API

--- routes/api.php
+++ routes/api.php
@@ -13,17 +13,21 @@ use Illuminate\Http\Request;
 |
 */
 
+Route::group(['as' => 'api.'], function () {
+    Route::post('/access_token', 'Api\AuthController@accessToken');
+    Route::post('/refresh_token', 'Api\AuthController@refreshToken');
 
-Route::post('/access_token', 'Api\AuthController@accessToken');
-Route::post('/refresh_token', 'Api\AuthController@refreshToken');
-Route::post('/logout', 'Api\AuthController@logout')->middleware('auth:api');
+    Route::group(['middleware' => 'auth:api'], function () {
+        Route::post('/logout', 'Api\AuthController@logout');
 
-Route::get('/hello-world', function (Request $request) {
-    return response()->json(['massage' => 'Hello Word']);
-})->middleware('auth:api');
+        Route::get('/hello-world', function (Request $request) {
+            return response()->json(['massage' => 'Hello Word']);
+        });
 
-Route::get('/user', function (Request $request){
-    $user = Auth::guard('api')->user();
-    //$user = $request->user('api');
-    return $user;
-})->middleware('auth:api');
+        Route::get('/user', function (Request $request) {
+            $user = Auth::guard('api')->user();
+            //$user = $request->user('api');
+            return $user;
+        });
+    });
+});
