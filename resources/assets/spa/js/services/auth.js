2e1020069787b8c97dcd2a5c417a7c915ea2a3ef
Ivan Santos <ivannildosanto@hotmail.com>
2017-06-05 00:05:15 -0300

    Criando logout

--- resources/assets/spa/js/services/auth.js
+++ resources/assets/spa/js/services/auth.js
@@ -12,10 +12,20 @@ const afterLogin = (response) => {
 export default {
     login(email, password){
         return Jwt.accessToken(email, password).then((response) => {
-            afterLogin(response);
             LocalStorege.set(TOKEN, response.data.token);
+            afterLogin(response);
             return response
         });
+    },
+    logout(){
+        let afterLogout = () => {
+            LocalStorege.remove(TOKEN);
+            LocalStorege.remove(USER);
+        };
+
+        return Jwt.logout()
+            .then(afterLogout())
+            .catch(afterLogout());
     },
     getAuthorizationHeader(){
         return `Bearer ${LocalStorege.get(TOKEN)}`;
