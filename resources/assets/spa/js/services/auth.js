c4d61466f42ab6f39396b95fbc250605d618f209
Ivan Santos <ivannildosanto@hotmail.com>
2017-06-04 23:18:01 -0300

    Método para pegar usuário

--- resources/assets/spa/js/services/auth.js
+++ resources/assets/spa/js/services/auth.js
@@ -1,15 +1,26 @@
 import {Jwt} from './resources';
 import LocalStorege from './localStorege';
+import {User} from '../services/resources';
 
 const TOKEN = 'token';
+const USER = 'user';
+
+const afterLogin = (response) => {
+    User.get().then((response) => LocalStorege.setObject(USER, response.data));
+};
+
 export default {
     login(email, password){
         return Jwt.accessToken(email, password).then((response) => {
+            afterLogin(response);
             LocalStorege.set(TOKEN, response.data.token);
             return response
         });
     },
     getAuthorizationHeader(){
         return `Bearer ${LocalStorege.get(TOKEN)}`;
+    },
+    user(){
+        return LocalStorege.getObject(USER);
     }
 }
