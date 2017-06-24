c3f2714f6ce2793ba642f9fc943467ef6da45f36
Ivan Santos <ivannildosanto@hotmail.com>
2017-06-24 00:00:36 -0300

    Mostrando mensagens de erro

--- resources/views/admin/banks/_form.blade.php
+++ resources/views/admin/banks/_form.blade.php
@@ -1,6 +1,8 @@
+
+<?php $errorClass = $errors->first('name') ? ['class' => 'validate invalid'] : []?>
 <div class="row">
     <div class="input-field col s6">
-        {!! Form::label('name', 'Nome') !!}
-        {!! Form::text('name', null) !!}
+        {!! Form::text('name', null, $errorClass) !!}
+        {!! Form::label('name', 'Nome', ['data-error' => $errors->first('name')]) !!}
     </div>
 </div>
